74fad2298378c29076455c6a00e159c51db6ea14
flexiblefactory <hal@Hals-MacBook-Pro.local>
2020-10-23 02:21:14 +0100

    Styling and error messages. Pagination.

--- tcs-fibonacci/src/App.js
+++ tcs-fibonacci/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import fibonacci from 'fibonacci-fast';
 import isComposite from '@extra-number/is-composite';
-const fibs = fibonacci.array(1, 18)
+const sequence = fibonacci.array(1, 18)
+const PAGE_SIZE = 10
 
 const SATURDAY = 6
 const SUNDAY = 7
@@ -13,7 +14,7 @@ function isWeekend() {
 }
 
 function DisplayInteger({ n }) {
-  const numberIsPrime = n.lte(Number.MAX_SAFE_INTEGER) && !isComposite(n)//!isComposite(n)
+  const numberIsPrime = !isComposite(n)
   const weekend = isWeekend()
   const primeLabel = weekend ? 'wic' : 'tic'
   const compositeLabel = weekend ? 'woe' : 'toe'
@@ -23,16 +24,44 @@ function DisplayInteger({ n }) {
 }
 function App() {
 
-  const [inputInt, setInputInt] = React.useState(10)
+  const [inputInt, setInputInt] = React.useState(2000)
+  const [page, setPage] = React.useState(1)
+  
   const userInput = parseInt(inputInt) || 0
-  const rangeError = !userInput || userInput <0   || userInput > 2000
+  const rangeError = !userInput || userInput < 0 || userInput > 2000
+  const fibs = sequence.filter(f => f.number <= userInput)
+  const totalPages = Math.ceil(fibs.length / PAGE_SIZE)
+  
+  const pageNum = Math.max(1, parseInt(page))
+  const skip = (pageNum-1)*PAGE_SIZE
+  const pageError = (!pageNum || pageNum < 0 || pageNum > totalPages)
 
   return (
     <div className="App">
-      {rangeError&& 'error'}      <input style={{ width: '45px' }} type="text" onChange={e => { setInputInt(e.target.value) }} value={inputInt}></input>
-      {userInput}
-      <div>{fibs.filter(f=>f.number<=userInput).map(f=><DisplayInteger n={f.number}></DisplayInteger>)}</div>
-      
+     
+      {pageError && <div className="error">Please enter a page number up to {totalPages}</div>}
+        {rangeError && <div className="error">Please enter an integer up to 2000</div>}
+        
+              <input style={{ width: '45px' }} type="text" onChange={e => { setInputInt(e.target.value) }} value={inputInt}></input>
+      <h1>Showing fibonacci sequence up to {userInput}</h1>
+
+      {
+          !pageError && !rangeError &&
+          <div>
+            <div>
+              <div>(page {pageNum} of {totalPages})</div>
+            </div>
+            <div>{fibs.slice(skip,skip+PAGE_SIZE).map(f => <DisplayInteger n={f.number}></DisplayInteger>)}</div>
+
+          </div>
+          }
+
+
+      <div>
+        <label>page</label><input style={{ width: '45px' }} type="text" onChange={e => { setPage(e.target.value) }} value={page}></input>
+      </div>
+      {pageNum !== 1 && <button onClick={() => setPage(pageNum - 1)}>previous page</button>}
+      {pageNum !== totalPages && <button onClick={() => setPage(pageNum + 1)}>next page</button>}
     </div>
   );
 }
