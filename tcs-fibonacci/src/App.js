28770b4f3717a126839587153ad80ebe720a99b8
flexiblefactory <hal@Hals-MacBook-Pro.local>
2020-10-23 01:58:39 +0100

    Step 2. Validation and Styling. Weekend check.

--- tcs-fibonacci/src/App.js
+++ tcs-fibonacci/src/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import fibonacci from 'fibonacci-fast';
 import isComposite from '@extra-number/is-composite';
+const fibs = fibonacci.array(1, 18)
 
+const SATURDAY = 6
+const SUNDAY = 7
+
+function isWeekend() {
+  const day = new Date().getDay()
+  return day === SATURDAY || day === SUNDAY
+}
+
+function DisplayInteger({ n }) {
+  const numberIsPrime = n.lte(Number.MAX_SAFE_INTEGER) && !isComposite(n)//!isComposite(n)
+  const weekend = isWeekend()
+  const primeLabel = weekend ? 'wic' : 'tic'
+  const compositeLabel = weekend ? 'woe' : 'toe'
+  return <div style={{ color: numberIsPrime ? 'blue' : 'green' }}>
+    {n.toString()} {numberIsPrime ? primeLabel : compositeLabel}
+  </div>
+}
 function App() {
 
   const [inputInt, setInputInt] = React.useState(10)
   const userInput = parseInt(inputInt) || 0
-  const rangeError = !userInput || userInput <0 
+  const rangeError = !userInput || userInput <0   || userInput > 2000
 
-  const fibs = fibonacci.array(0, userInput)
   return (
     <div className="App">
       {rangeError&& 'error'}      <input style={{ width: '45px' }} type="text" onChange={e => { setInputInt(e.target.value) }} value={inputInt}></input>
       {userInput}
-      <div>{fibs.map(f=><div>{f.number.toString() + ' '+ (isComposite(f.number) ? 'composite':'prime')}</div>)}</div>
+      <div>{fibs.filter(f=>f.number<=userInput).map(f=><DisplayInteger n={f.number}></DisplayInteger>)}</div>
       
     </div>
   );
